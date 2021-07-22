e71619ff08023f8c55d864446e1818d60128c002
Thiago Kachinsky <tjk091@gmail.com>
2021-07-22 10:57:02 -0300

    Consumindo API e inicio da estilização dos cards

---
+++ src/components/Card/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 10px;
+
+  .card {
+    display: flex;
+  }
+
+  img {
+
+  }
+
+  .infos {
+
+  }
+
+  h1 {
+    font-size: 20px;
+  }
+
+  .original_price {
+    
+  }
+
+  p {
+
+  }
+`;
